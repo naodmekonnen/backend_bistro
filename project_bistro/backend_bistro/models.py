cd005295aede9768b399157e4db1b0b6d2eeb406
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-09 19:51:10 +0000

    domestic_key

--- project_bistro/backend_bistro/models.py
+++ project_bistro/backend_bistro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=50)
+
+class Menu_Items(models.Model):
+    entree = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    spice_level = models.IntegerField(max_length=50)
+    price = models.IntegerField(max_length=50)
