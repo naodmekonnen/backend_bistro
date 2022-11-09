4361641afe6d47f8dcba7655d24b5f93ba99a717
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-09 21:30:25 +0000

    menu_items populated

--- project_bistro/backend_bistro/models.py
+++ project_bistro/backend_bistro/models.py
@@ -2,15 +2,22 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    def __str__ (self):
+        return self.name
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=50)
+    def __str__ (self):
+        return self.name
 
 
 class Menu_Items(models.Model):
     entree = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
     spice_level = models.IntegerField(max_length=50)
     price = models.IntegerField(max_length=50)
     cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__ (self):
+        self.entree + ' ' + ' ' + desc
