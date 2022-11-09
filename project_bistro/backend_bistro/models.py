9202d14f1bdd7ce956a9a4139c43411f21ac7fa5
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-09 20:05:23 +0000

    models

--- project_bistro/backend_bistro/models.py
+++ project_bistro/backend_bistro/models.py
@@ -6,8 +6,11 @@ class Category(models.Model):
 class Cuisine(models.Model):
     name = models.CharField(max_length=50)
 
+
 class Menu_Items(models.Model):
     entree = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     spice_level = models.IntegerField(max_length=50)
     price = models.IntegerField(max_length=50)
+    cuisine = models.ForeignKey(Cuisine)
+    category = models.ForeignKey(Category)
