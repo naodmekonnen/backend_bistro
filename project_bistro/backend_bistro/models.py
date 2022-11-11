051b9cad0190ae158d24be61e9cbb10f46bd691d
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-11 14:29:24 +0000

    SLUG

--- project_bistro/backend_bistro/models.py
+++ project_bistro/backend_bistro/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -12,16 +14,24 @@ class Cuisine(models.Model):
 
 
 
-class Menu_Items(models.Model):
+class MenuItem(models.Model):
     entree = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
     spice_level = models.IntegerField(max_length=50)
     price = models.IntegerField(max_length=50)
     cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__ (self):
         return self.entree + ' ' + self.description
+
+    def save(self, *args, **kwargs):
+        # self.slug = slugify(self.entree)
+        super().save(*args, **kwargs)
+        if self.slug is None:
+            self.slug = slugify(self.entree)
+            self.save()
 
     def json(self):
         return{
--- project_bistro/backend_bistro/views.py
+++ project_bistro/backend_bistro/views.py
@@ -1,38 +1,39 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views import View
-from . models import Category,Cuisine,Menu_Items
+from . models import Category,Cuisine,MenuItem
 from django.core.serializers import serialize
 import json
 import csv
 
 
-
+# fetch and return json serialized data from API
 def get_data(request):
     data = [
-            i.json() for i in Menu_Items.objects.all()
+            i.json() for i in MenuItem.objects.all()
         ]
     return HttpResponse(json.dumps(data), content_type='application/json')
 
+
 def indian(request):
-    indian = list(Menu_Items.objects.filter(cuisine=3).values())
+    indian = list(MenuItem.objects.filter(cuisine=3).values())
     return JsonResponse(indian, safe=False)
 
 def american(request):
-    american = list(Menu_Items.objects.filter(cuisine=1).values())
+    american = list(MenuItem.objects.filter(cuisine=1).values())
     return JsonResponse(american, safe=False)
 
 def italian(request):
-    italian = list(Menu_Items.objects.filter(cuisine=2).values())
+    italian = list(MenuItem.objects.filter(cuisine=2).values())
     return JsonResponse(italian, safe=False)
 
 def mediterranean(request):
-    mediterranean = list(Menu_Items.objects.filter(cuisine=5).values())
+    mediterranean = list(MenuItem.objects.filter(cuisine=5).values())
     return JsonResponse(mediterranean, safe=False)
 
 
 def export_to_csv(request):
-    items = Menu_Items.objects.all()
+    items = MenuItem.objects.all()
     response = HttpResponse(content_type='text/csv')
     writer = csv.writer(response)
     writer.writerow(['entree','description','spice_level','price','cuisine','category'])
